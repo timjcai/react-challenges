4230dd5b72c38af99a3a198eb38c795114e75c7a
Tim Cai <tim.j.cai@gmail.com>
2023-05-12 18:23:18 +1000

    created basic app that implements a GET request call - using useEffect Hook

--- 03-useEffect/01-basic-page-nav/src/App.js
+++ 03-useEffect/01-basic-page-nav/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [page, setPage] = useState('posts')
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    console.log('changed page')
+    fetch(`http://jsonplaceholder.typicode.com/${page}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+    // when we change navigation we want to call useEffect to pull information from an API to generate new content
+  }, [page])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={e => setPage('posts')}>Posts</button>
+      <button onClick={e => setPage('comments')}>Comments</button>
+      <button onClick={e => setPage('users')}>Users</button>
+      <button onClick={e => setPage('todos')}>Todos</button>
+      <h1> {page} </h1>
+      <div>
+        {items.map(item => {
+          return <pre> {JSON.stringify(item)} </pre>
+        })}
+      </div>
     </div>
   );
 }
