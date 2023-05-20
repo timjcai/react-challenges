1bc059994cfc7d638ba921343b89f2ece633a75a
Tim Cai <tim.j.cai@gmail.com>
2023-05-20 11:18:16 +1000

    successfully fetched all the data from the todo backend for index page - next up: post request, delete request, patch request

--- 03-useEffect/07-MERN-Todoapp/frontend/src/App.js
+++ 03-useEffect/07-MERN-Todoapp/frontend/src/App.js
@@ -2,13 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Create from './components/Create'
 import Task from './components/Task';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    const url = `http://localhost:3000/api/v1/todos`
+
+    fetch(url)
+      .then(response => response.json())
+      // .then(data => console.log(data))
+      .then(data => setTodos(data))
+      .catch(error => console.log(error))
+  }, [todos])
+
+  const todoList = todos.map((task) => (
+    < Task props = {task} />
+  ))
+
   return (
     <div className="App">
       <header className="App-header">
         <Create />
-        <Task />
+        <> { todoList }</>
       </header>
     </div>
   );
--- 03-useEffect/07-MERN-Todoapp/frontend/src/components/Task.js
+++ 03-useEffect/07-MERN-Todoapp/frontend/src/components/Task.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/style.css'
 import { ImBin2, ImPencil } from 'react-icons/im'
 import { IoIosSave } from 'react-icons/io'
@@ -6,27 +6,31 @@ import { useState } from 'react'
 import { BsFillExclamationCircleFill, BsFillCheckCircleFill } from 'react-icons/bs'
 
 function Task(props) {
-  const { id, completed: taskCompletionStatus, title, tag } = props
+  const { _id, completed: taskCompletionStatus, title, tag } = props.props
   const [completed, setCompleted] = useState(`${taskCompletionStatus}`)
   const [editMode, setEditMode] = useState(false)
 
+  useEffect(() => {
+    setCompleted(taskCompletionStatus)
+  }, [])
+
   return (
     <div className="task-block">
-      <div>
-        <div onClick={(e) => {setCompleted(!completed) }} className="completed" data-id={id}>
-          {completed && <BsFillExclamationCircleFill />}
-          {!completed && <BsFillCheckCircleFill />}
+      <div className="list-item">
+        <div onClick={(e) => {setCompleted(!completed) }} className="completed" data-id={_id}>
+          {!completed && <BsFillExclamationCircleFill />}
+          {completed && <BsFillCheckCircleFill />}
         </div>
         <div className="todo-wrapper">
           <input className="todo title" value={ title }></input>
           <input className="todo tag" value={ tag }></input>
         </div>
         <div className="action-buttons">
-          <button onClick={(e) => { setEditMode(!editMode) }}>
+          <a className="edit" onClick={(e) => { setEditMode(!editMode) }}>
             { !editMode && <ImPencil />}
             { editMode && <IoIosSave /> }
-          </button>
-          <button><ImBin2 /></button>
+          </a>
+          <a className="delete"><ImBin2 /></a>
         </div>
       </div>
     </div>
