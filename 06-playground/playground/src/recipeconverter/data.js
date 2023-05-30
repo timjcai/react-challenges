862168fb8b246d3c44e667544e0798679a4266d7
Tim Cai <tim.j.cai@gmail.com>
2023-05-30 15:53:51 +1000

    created plain data to play with for caloriecounting app

---
+++ 06-playground/playground/src/recipeconverter/data.js
@@ -0,0 +1,72 @@
+export const data = [
+  {
+    title: "Grilled Chicken with Sweet Potato and Broccoli",
+    ingredients: [
+      // ["Name", "Units", "Unit measurement", "Weight", "Weight measurement", "Comments/Description"]
+      ["Chicken breast", 4, "pieces", 680, "g", "Boneless"],
+      ["Sweet potato", 2, "potatoes", 450, "g", "Peeled and cubed"],
+      ["Broccoli florets", 2, "cups", 141.95, "g", "florets only"],
+      ["Olive oil", 2, "tablespoons", 29.57, "ml", "any brand"],
+      ["Garlic", 3, "cloves", 10, "g", "minced"],
+      ["Paprika", 1, "teaspoon", 10, "g", "to taste"],
+      ["Salt", 1, "teaspoon", 10, "g", "to taste"],
+      ["Pepper", 1, "teaspoon", 10, "g", "to taste"],
+    ],
+    instructions: {
+      step1: "Preheat your grill to medium-high heat.",
+      step2: "In a bowl, mix together olive oil, garlic powder, paprika, salt, and pepper.",
+      step3: "Brush the chicken breasts with the olive oil mixture and place them on the grill. Cook for about 6-8 minutes per side or until cooked through.",
+      step4: "In the meantime, steam the sweet potatoes and broccoli until tender.",
+      step5: "Divide the grilled chicken, sweet potatoes, and broccoli into meal prep containers.",
+      step6: "Let them cool before sealing the containers. Store in the refrigerator and reheat when ready to eat."
+    }
+  },
+  {
+    title: "Beef Stir-Fry with Brown Rice",
+    ingredients: [
+      // ["Name", "Units", "Unit measurement", "Weight", "Weight measurement", "Comments/Description"]
+      ["Lean Beef", 1, "container", 450, "g", "Sirloin or flank steak, thinly sliced"],
+      ["Carrots", 2, "carrots", 70, "g", "Peeled and cubed"],
+      ["Broccoli florets", 1, "cup(s)", 70.5, "g", "florets only"],
+      ["Brown Rice", 1, "cup(s)", 185, "g", "cooked"],
+      ["Soy sauce", 2, "tablespoon(s)", 29.57, "ml", "your preference - low sodium or regular"],
+      ["Sesame Oil", 1, "tablespoon(s)", 14, "ml", "any brand"],
+      ["Garlic", 3, "cloves", 10, "g", "minced"],
+      ["Corn starch", 1, "tablespoon(s)", 14, "mg", "any brand"],
+      ["Salt", 1, "teaspoon", 10, "g", "to taste"],
+      ["Pepper", 1, "teaspoon", 10, "g", "to taste"],
+    ],
+    instructions: {
+      step1: "In a small bowl, whisk together soy sauce, sesame oil, cornstarch, minced garlic, salt, and pepper.",
+      step2: "Heat a large skillet or wok over medium-high heat and add the beef slices. Cook until browned on all sides, then remove the beef from the skillet.",
+      step3: "In the same skillet, add the mixed stir-fry vegetables and cook until slightly tender.",
+      step4: "Return the beef to the skillet and pour the soy sauce mixture over it. Stir-fry for a few minutes until the sauce thickens and coats the beef and vegetables.",
+      step5: "Divide the beef stir-fry and brown rice into meal prep containers.",
+      step6: "Allow them to cool before closing the containers. Store in the refrigerator and reheat when ready to eat."
+    }
+  },
+  {
+    title: "Baked Salmon with Quinoa and Roasted Vegetables",
+    ingredients: [
+      // ["Name", "Units", "Unit measurement", "Weight", "Weight measurement", "Comments/Description"]
+      ["Salmon", 4, "fillets", 680, "g", "Boneless"],
+      ["Quinoa", 1, "cup(s)", 185, "g", "cooked, any color: white, brown, mixed etc"],
+      ["Carrots", 2, "carrots", 70, "g", "Peeled and cubed"],
+      ["Broccoli florets", 1, "cup(s)", 70.5, "g", "florets only"],
+      ["Olive oil", 2, "tablespoons", 29.57, "ml", "any brand"],
+      ["Garlic", 3, "cloves", 10, "g", "minced"],
+      ["Dried Herb", 1, "teaspoon", 10, "g", "mixed herb - or variations of thyme, rosemary, basil - decide depending on your flavour preference"],
+      ["Salt", 1, "teaspoon", 10, "g", "to taste"],
+      ["Pepper", 1, "teaspoon", 10, "g", "to taste"],
+    ],
+    instructions: {
+      step1: "Preheat your oven to 400°F (200°C).",
+      step2: "Cook the quinoa according to the package instructions.",
+      step3: "Place the salmon fillets on a baking sheet lined with parchment paper. Drizzle with olive oil and sprinkle with dried herbs, salt, and pepper.",
+      step4: "Bake the salmon for about 12-15 minutes or until cooked to your desired doneness.",
+      step5: "In the meantime, toss the mixed vegetables with olive oil, salt, and pepper. Spread them on a separate baking sheet and roast in the oven for 15-20 minutes or until tender.",
+      step6: "Divide the baked salmon, quinoa, and roasted vegetables into meal prep containers.",
+      step7: "Allow them to cool before sealing the containers. Store in the refrigerator and reheat when ready to eat."
+    }
+  }
+]
